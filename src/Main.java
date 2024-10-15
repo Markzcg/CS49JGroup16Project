1e7f573a5693c58bdbeaa3ac211f8bd1062bfcba
Markzcg <zhaochenge666@gmail.com>
2024-10-14 22:13:32 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Appointment appointment = new Appointment(LocalDate.of(2024, 10, 10), LocalDate.of(2024, 10, 20));
 
+        LocalDate testDate1 = LocalDate.of(2024, 10, 9);
+        LocalDate testDate2 = LocalDate.of(2024, 10, 10);
+        LocalDate testDate3 = LocalDate.of(2024, 10, 15);
+        LocalDate testDate4 = LocalDate.of(2024, 10, 20);
+        LocalDate testDate5 = LocalDate.of(2024, 10, 21);
+
+        System.out.println("Test 1 (Before Start Date): " + appointment.occursOn(testDate1));
+        System.out.println("Test 2 (On Start Date): " + appointment.occursOn(testDate2));
+        System.out.println("Test 3 (Between Start and End Date): " + appointment.occursOn(testDate3));
+        System.out.println("Test 4 (On End Date): " + appointment.occursOn(testDate4));
+        System.out.println("Test 5 (After End Date): " + appointment.occursOn(testDate5));
     }
 }
