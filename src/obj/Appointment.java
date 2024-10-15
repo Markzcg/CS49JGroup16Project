3658ed0d3f69993cd2ac11629bc78076e29aa2b8
Markzcg <zhaochenge666@gmail.com>
2024-10-14 21:04:33 -0700

    Update Appointment.java

--- src/obj/Appointment.java
+++ src/obj/Appointment.java
@@ -1,4 +1,26 @@
 package obj;
+import java.time.LocalDate;
 
 public class Appointment {
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Appointment(LocalDate startDate, LocalDate endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean occursOn(LocalDate date) {
+        return (date.isEqual(startDate) || date.isEqual(endDate) || 
+               (date.isAfter(startDate) && date.isBefore(endDate)));
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 }
+
