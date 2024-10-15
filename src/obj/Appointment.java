8368ef7275e51fae5a9dda801981e8730a02bcb9
Your Name <yourname@example.com>
2024-10-14 21:55:21 -0700

    Adding Appointment/AppointmentTest classes and folders

--- src/obj/Appointment.java
+++ src/obj/Appointment.java
@@ -1,25 +1,26 @@
 package obj;
 
 import java.time.LocalDate;
-
 public class Appointment {
+    private String description;
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public Appointment(LocalDate startDate, LocalDate endDate) {
+    public Appointment(String description, LocalDate startDate, LocalDate endDate) {
+        this.description = description;
         this.startDate = startDate;
         this.endDate = endDate;
     }
-
     public boolean occursOn(LocalDate date) {
-        return (date.isEqual(startDate) || date.isEqual(endDate) || 
-               (date.isAfter(startDate) && date.isBefore(endDate)));
+        return (date.isEqual(startDate) || date.isEqual(endDate) ||
+                (date.isAfter(startDate) && date.isBefore(endDate)));
     }
-
+    public String getDescription() {
+        return description;
+    }
     public LocalDate getStartDate() {
         return startDate;
     }
-
     public LocalDate getEndDate() {
         return endDate;
     }
--- src/tests/AppointmentTest.java
+++ src/tests/AppointmentTest.java
@@ -1,4 +1,68 @@
 package tests;
+import obj.Appointment;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+
+
 
 public class AppointmentTest {
+    private Appointment appointment;
+
+    @Before
+    public void setup(){
+        LocalDate start = LocalDate.of(2024, 10, 10);
+        LocalDate end = LocalDate.of(2024, 10, 20);
+        appointment = new Appointment("test", start, end);
+    }
+    @Test
+    public void testBeforeStartDate(){
+        // < startDate && < endDate = false;
+        LocalDate checkDate = LocalDate.of(2024, 9, 10);
+        boolean actual = appointment.occursOn(checkDate);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOnStartDate(){
+        // = startDate && < endDate = true;
+        LocalDate checkDate = LocalDate.of(2024, 10, 10);
+        boolean actual = appointment.occursOn(checkDate);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testBetweenStartAndEndDate(){
+        // > startDate && < endDate = true;
+        LocalDate checkDate = LocalDate.of(2024, 10, 15);
+        boolean actual = appointment.occursOn(checkDate);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOnEndDate(){
+        // > startDate && = endDate = true;
+        LocalDate checkDate = LocalDate.of(2024, 10, 20);
+        boolean actual = appointment.occursOn(checkDate);
+        boolean expected = true;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAfterEndDate(){
+        // > startDate && = endDate = true;
+        LocalDate checkDate = LocalDate.of(2024, 10, 21);
+        boolean actual = appointment.occursOn(checkDate);
+        boolean expected = false;
+        assertEquals(expected, actual);
+    }
+
+
+
 }
