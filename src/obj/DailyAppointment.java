790b0c4fe847fa9f8bb32b5a8eeb7123b03e2b71
Markzcg <zhaochenge666@gmail.com>
2024-11-04 18:09:29 -0800

    DailyAppointment.java

--- src/obj/DailyAppointment.java
+++ src/obj/DailyAppointment.java
@@ -1,1 +1,17 @@
+import java.time.LocalDate;
 
+public class DailyAppointment extends Appointment {
+    public DailyAppointment(String description, LocalDate startDate, LocalDate endDate) {
+        super(description, startDate, endDate);
+    }
+
+    @Override
+    public boolean occursOn(LocalDate date) {
+        return inBetween(date);
+    }
+
+    @Override
+    public String toString() {
+        return "Daily Appointment: " + getDescription() + ", Start Date: " + getStartDate() + ", End Date: " + getEndDate();
+    }
+}
