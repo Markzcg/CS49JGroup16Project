af741f378b4a3ff63c5a1a767d90e332427f503d
Markzcg <zhaochenge666@gmail.com>
2024-11-04 18:08:23 -0800

    OnetimeAppointment.java

--- src/obj/OnetimeAppointment.java
+++ src/obj/OnetimeAppointment.java
@@ -1,1 +1,18 @@
+import java.time.LocalDate;
+
+public class OnetimeAppointment extends Appointment {
+    public OnetimeAppointment(String description, LocalDate date) {
+        super(description, date, date);
+    }
+
+    @Override
+    public boolean occursOn(LocalDate date) {
+        return date.isEqual(getStartDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Onetime Appointment: " + getDescription() + ", Date: " + getStartDate();
+    }
+}
 
