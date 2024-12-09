a05c776959454c34c923d6504ef62ff2815fad2c
CI-IE <vcheng3264@gmail.com>
2024-12-08 17:29:02 -0800

    updated class and descriptor names
    changed class and descriptor names to proper coding conventions.

--- src/obj/DescriptionComparator.java
+++ src/obj/DescriptionComparator.java
@@ -2,7 +2,7 @@ package obj;
 
 import java.util.Comparator;
 
-public class descriptionComparator implements Comparator<Appointment> {
+public class DescriptionComparator implements Comparator<Appointment> {
     @Override
     public int compare(Appointment o1, Appointment o2) {
         return o1.getDescription().compareTo(o2.getDescription());
