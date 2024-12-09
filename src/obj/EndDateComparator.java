d3fa58939a4cd70b0fe5a1aa2f6cda35ee89e0bd
CI-IE <vcheng3264@gmail.com>
2024-12-08 17:30:07 -0800

    updated file and class names
    renamed file and class names to proper coding conventions.

--- src/obj/EndDateComparator.java
+++ src/obj/EndDateComparator.java
@@ -2,7 +2,7 @@ package obj;
 
 import java.util.Comparator;
 
-public class endDateComparator implements Comparator<Appointment> {
+public class EndDateComparator implements Comparator<Appointment> {
     @Override
     public int compare(Appointment o1, Appointment o2) {
         return o1.getEndDate().compareTo(o2.getEndDate());
